d4e951f49e7565a24f529f9f7302e9fee8cb4e0a
Markus Hsi-Yang Fritz <markus.hsi.yang@gmail.com>
2014-07-01 22:22:27 +0200

    added defaults

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var defaultEq = '\\sum_i P(x_i) \\log_2 P(x_i)';
+var defaultFontSize = 30;
+var defaultBgColor = '#ffffff';
+var defaultFgColor = '#000000';
 
 $(document).ready(function() {
   showDefault();
@@ -7,9 +10,28 @@ $(document).ready(function() {
 function showDefault() {
   $("textarea").val(defaultEq);
   $("#displayarea").html(delimit(defaultEq));
+  $("#fontsize").val(defaultFontSize);
+  $("input[name='bg-color']").val(defaultBgColor);
+  $("input[name='fg-color']").val(defaultFgColor);
   MathJax.Hub.Queue(["Typeset", MathJax.Hub, "displayarea"]);
+  MathJax.Hub.Queue(setFontSize);
+  MathJax.Hub.Queue(setBgColor(defaultBgColor));
+  MathJax.Hub.Queue(setFgColor(defaultFgColor));
 }
 
 function delimit(s) {
   return '\\[' + s + '\\]';
+}
+
+function setFontSize() {
+  var fontSize = parseInt($('#fontsize').val(), 10);
+  $('#displayarea').css('font-size', fontSize);
+}
+
+function setBgColor(col) {
+  $("#displayarea").css("background-color", col);
+}
+
+function setFgColor(col) {
+    $("#displayarea").css("color", col);
 }
